9bcbca49ff2cd17587900f9c152b8bed795ac51d
etboi1 <et.grundvig@gmail.com>
2025-11-05 16:06:35 -0700

    Added all ten questions and answer queries to quizQuestions.js

--- quizQuestions.js
+++ quizQuestions.js
@@ -122,13 +122,13 @@ module.exports = [
         id: 6,
         question: "What order of the Knights Radiant does Dalinar Kholin belong to?",
         correctQuery: `
-            SELECT cma.description
+            SELECT cma.description AS answer
             FROM characters c
             INNER JOIN character_magic_abilities cma ON c.character_id = cma.character_id
             WHERE c.char_first_name = 'Dalinar';
         `,
         wrongQuery: `
-            SELECT DISTINCT cma.specific_abilities
+            SELECT DISTINCT cma.specific_abilities AS answer
             FROM characters c
             INNER JOIN character_magic_abilities cma ON c.character_id = cma.character_id
             INNER JOIN magic_systems ms ON cma.magic_system_id = ms.magic_system_id
@@ -137,6 +137,81 @@ module.exports = [
                 AND cma.specific_abilities NOT LIKE '%Spren%'
                 AND cma.specific_abilities NOT LIKE '%,%'
                 AND cma.specific_abilities NOT LIKE 'ing'
+            ORDER BY RAND()
+            LIMIT 3;
+        `
+    },
+    {
+        id: 7,
+        question: "What is the real name of 'Painter' in Yumi and the Nightmare Painter?",
+        correctQuery: `
+            SELECT 'Nikaro' AS answer;
+        `,
+        wrongQuery: `
+            SELECT c.char_first_name AS answer
+            FROM characters c
+            INNER JOIN planets p ON c.planet_of_origin = p.planet_id
+            WHERE p.planet_name IN ('Komashi', 'Sel')
+                AND c.char_first_name NOT IN  ('Nikaro (Painter)', 'Yumi')
+            ORDER BY RAND()
+            LIMIT 3;
+        `
+    },
+    {
+        id: 8,
+        question: "What is the name of the Emperor in 'The Emperor's Soul'?",
+        correctQuery: `
+            SELECT 'Ashravan' AS answer;
+        `,
+        wrongQuery: `
+            SELECT c.char_first_name AS answer
+            FROM characters c
+            INNER JOIN planets p ON c.planet_of_origin = p.planet_id
+            WHERE p.planet_name IN ('Sel', 'Nalthis', 'Yolen')
+                AND c.char_first_name NOT IN ('Hoid (Wit)', 'Yumi', 'Nikaro (Painter)', 'Ashravan', 'Blushweaver', 'Nightblood', 'Uli')
+                AND c.char_first_name NOT LIKE '%(%'
+            ORDER BY RAND()
+            LIMIT 3;
+        `
+    },
+    {
+        id: 9,
+        question: "Which magic system can Hoid (Wit) NOT use?",
+        correctQuery: `
+            SELECT system_name AS answer
+            FROM magic_systems
+            WHERE magic_system_id NOT IN (
+                SELECT cma.magic_system_id
+                FROM characters c
+                INNER JOIN character_magic_abilities cma ON c.character_id = cma.character_id
+                WHERE c.char_first_name = 'Hoid (Wit)'
+            )
+                AND system_name NOT IN ('Hion', 'Yoki-hijo', 'Nightmar Painters')
+            ORDER BY RAND()
+            LIMIT 1;
+        `,
+        wrongQuery: `
+            SELECT ms.system_name AS answer
+            FROM characters c
+            INNER JOIN character_magic_abilities cma ON c.character_id = cma.character_id
+            INNER JOIN magic_systems ms ON cma.magic_system_id = ms.magic_system_id
+            WHERE c.char_first_name = 'Hoid (Wit)'
+            ORDER BY RAND()
+            LIMIT 3;
+        `
+    },
+    {
+        id: 10,
+        question: "What is AonDor?",
+        correctQuery: `
+            SELECT description
+            FROM magic_systems
+            WHERE system_name = 'AonDor';
+        `,
+        wrongQuery: `
+            SELECT description
+            FROM magic_systems
+            WHERE system_name NOT IN ('AonDor', 'Allomancy', 'Bloodsealing', 'Feruchemy', 'Hemalurgy', 'Surgebinding', 'Navigation', 'Yolish Lightweaving')
             ORDER BY RAND()
             LIMIT 3;
         `
